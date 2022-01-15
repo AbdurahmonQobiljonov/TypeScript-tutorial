6fe0da6879a4d8be850b501211b9f375c6ef4f84
Abdurakhmon Kobiljonov <abdurakhmon_kobiljonov@epam.com>
2022-01-15 12:58:10 +0500

    Updates

--- src/components/charList/CharListProps.ts
+++
@@ -1,3 +0,0 @@
-export interface  onCharSelect  {
-    onCharSelected:(id:number|undefined )=>void
-}
--- src/components/errorMassage/ErrorMassage.tsx
+++
@@ -1,8 +0,0 @@
-const ErrorMassage = (): JSX.Element => {
-    return <img
-        src='/error.gif'
-        alt='Error'
-        style={{display:'block',width:'250px',height:'250px',objectFit:'contain',margin:'0 auto'}}
-    />
-}
-export default ErrorMassage;
